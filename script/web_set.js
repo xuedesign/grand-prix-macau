b0144dc38759113423a87e0d9b896512af1de95a
xuedesign <elmo@xuedesign.tw>
2014-09-10 16:07:26 +0800

    頁面滾動架構完成 #20140910

--- script/web_set.js
+++ script/web_set.js
@@ -15,5 +15,37 @@ window.onload = externalLinks;
 $(function() {
   $("#page_container img").lazyload({placeholder : "../images/grey.gif",effect: "fadeIn"});
       });
-	  	  
+	  
+//選單設定
+$(document).ready(function() {
+	  $('#fullpage').fullpage({
+		  sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
+		  anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
+		  menu: '#menu',
+		  afterLoad: function(anchorLink, index){
+
+			  //section 2
+			  if(index == 2){
+				  //moving the image
+				  $('#section1').find('img').delay(500).animate({
+					  left: '0%'
+				  }, 1500, 'easeOutExpo');
+
+				  $('#section1').find('p').first().fadeIn(1800, function(){
+					  $('#section1').find('p').last().fadeIn(1800);
+				  });;
+
+			  }
+
+			  //section 3
+			  if(anchorLink == '3rdPage'){
+				  //moving the image
+				  $('#section2').find('.intro').delay(500).animate({
+					  left: '0%'
+				  }, 1500, 'easeOutExpo');
+			  }
+		  }
+	  });
+
+  });	  	  
 	    	  
