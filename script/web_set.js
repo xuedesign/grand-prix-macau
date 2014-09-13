6eb9cd3c0851ed87a240cdfd1d61a92e36e7a336
xuedesign <elmo@xuedesign.tw>
2014-09-14 03:25:40 +0800

    旅行社按鈕調整 #20140914

--- script/web_set.js
+++ script/web_set.js
@@ -23,29 +23,7 @@ $(document).ready(function() {
 		  anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
 		  menu: '#menu',
 		  afterLoad: function(anchorLink, index){
-
-			  	  
-			  //section 2
-			  if(index == 2){
-				  //moving the image
-				  $('#section1').find('img').delay(500).animate({
-					  left: '0%'
-				  }, 1500, 'easeOutExpo');
-
-				  $('#section1').find('p').first().fadeIn(1800, function(){
-					  $('#section1').find('p').last().fadeIn(1800);
-				  });;
-
-			  }
-
-			  //section 3
-			  if(anchorLink == '3rdPage'){
-				  //moving the image
-				  $('#section2').find('.intro').delay(500).animate({
-					  left: '0%'
-				  }, 1500, 'easeOutExpo');
-			  }
-		  }
+          }
 	  });
 
   });
